e2270059dd05014970a4206c31dace22ed747587
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-01-31 11:30:01 +0530

    Javascript Live DOM and Assignment 1 uploaded

--- Projects/JS assignments/Assignment 1/question_24.js
+++ Projects/JS assignments/Assignment 1/question_24.js
@@ -1,25 +1,14 @@
 // 24. Write a program to create two array "even" and "odd" having even and odd number between the 0 to 100 (0 and 100 included).
-function getEvenNumbers() {
-    let arr = [];
-    let evenArr = [];
-    let oddArr = [];
-    for (let i = 0; i <= 100; i++) {
-        arr.push(i);
-        // arr[i] % 2 === 0 ? evenArr = arr[i] : oddArr = arr[i]
-        // console.log(evenArr);
-        if (arr[i] % 2 === 0) {
-            evenArr = arr.filter(i);
-            console.log(evenArr);
-        }
-        else{
-            // oddArr = arr[i];
-            // console.log(oddArr);
-        }
-    }
+let arr = [];
+let even = [];
+let odd = [];
+for (let i = 0; i<=100; i++){
+    arr.push(i);
+    even = arr.filter(i => i%2==0);
+    odd = arr.filter (i => i%2 !==0);
 }
-getEvenNumbers();
-
-// console.log(evenArr);
+console.log("Even array is :",even);
+console.log("Odd array is :",odd);
 
 
 
