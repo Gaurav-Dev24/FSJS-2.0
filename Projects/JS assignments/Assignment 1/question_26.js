c560529f95f8d8720b2057cfbe396bde61359a7b
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-01-31 12:32:22 +0530

    26th question solved in js assignment

---
+++ Projects/JS assignments/Assignment 1/question_26.js
@@ -0,0 +1,23 @@
+// 26. Write a program to print the table of any number given by the user. The format of the output should be smiliar to the below example-
+// 	If the number given by the user is 2 then the output should look like this-
+// 	2 * 1 = 2
+// 	2 * 2 = 4
+// 	2 * 3 = 6 and so on till 2 * 10 = 20.
+
+let table = (num)=>{
+    for(let i = 1; i <= 10 ; i++){
+        console.log(`${num} * ${i} =`, num *i);
+    }
+}
+table(2);
+// o/p -- 
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+// 2 * 5 = 10
+// 2 * 6 = 12
+// 2 * 7 = 14
+// 2 * 8 = 16
+// 2 * 9 = 18
+// 2 * 10 = 20
