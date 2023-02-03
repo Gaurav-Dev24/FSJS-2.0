415e4da0c1351d4a12b943cc6a0400a7ccc70445
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-02-03 13:51:13 +0530

    29 question solved and added

--- Projects/JS assignments/Assignment 1/question_29.js
+++ Projects/JS assignments/Assignment 1/question_29.js
@@ -1,2 +1,8 @@
 // 29. Write a javascript program which takes the input of filename as string and prints the extension of the file in the console.
-
+let getExtension = (filename) =>{
+    let extension = filename.split('.').pop();
+    console.log(extension);
+}
+getExtension('module.html')
+// o/p--
+// html
